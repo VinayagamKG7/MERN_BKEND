de1a1370414fff7504403166b35cda40587605c0
VinayagamKG7 <kvinayagam.dev@gmail.com>
2026-09-09 10:43:00 +0530

    Day1 Process Updated

---
+++ JS/DAY1/script.js
@@ -0,0 +1,24 @@
+var firstName;
+var lastName;
+var userName;
+var userAge;
+var phoneNumber;
+var emailAddress;
+var homeAddress;
+var studentName;
+var studentAge;
+var studentMark;
+
+let totalMarks;
+let averageMark;
+let employeeName;
+let employeeId;
+let employeeSalary;
+let employeeDepartment;
+let productName;
+let productPrice;
+let productQuantity;
+let totalPrice;
+
+const studentMark = 90 ;
+console.log( studentMark ) ;
